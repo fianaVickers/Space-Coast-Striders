b86e3d6e3c132c562aa966ed938177a414cfafd4
fianaVickers <fiana.vickers@ufl.edu>
2024-08-28 20:07:33 -0400

    finished design of contact us page

--- scs/app/contactPage/page.tsx
+++ scs/app/contactPage/page.tsx
@@ -1,37 +1,25 @@
 import { NavBars } from "@/components/ui/HeaderNavBar";
 import { ShootingStarsAndStarsBackgroundDemo } from "@/components/ui/shootingStarts";
 import { FooterComp } from "@/components/ui/footer";
+import { SignupFormDemo } from "@/components/ui/emailForm";
 export default function contactPage() {
     return (
-      <div>
-        <NavBars></NavBars>
-        <ShootingStarsAndStarsBackgroundDemo content={"Connect with the Striders"}></ShootingStarsAndStarsBackgroundDemo>
-        
-        <div className="h-[3rem] pt-40 flex justify-center items-center">
-          <div className="text-4xl font-bold text-neutral-700">
-            Email Coach Vickers
-          </div>
-        </div>
-        
-        <div className="h-[3rem] pt-40 flex justify-center items-center">
-          <div className="text-4xl font-bold text-neutral-700">
-            Join the GroupMe
-          </div>
-        </div>
+        <div>
+            <NavBars></NavBars>
+            <ShootingStarsAndStarsBackgroundDemo content={"Connect with the Striders"}></ShootingStarsAndStarsBackgroundDemo>
 
-        <div className="h-[3rem] pt-40 flex justify-center items-center">
-          <div className="text-4xl font-bold text-neutral-700">
-            Checkout our Facebook Page
-          </div>
+            <div className="h-[10rem] p-30 flex justify-center items-center">
+                <div className="text-5xl font-bold text-neutral-700">
+                    We appreciate your interest!
+                </div>
+            </div>
+
+            <SignupFormDemo></SignupFormDemo>
+            <div className="h-100">
+
+            </div>
+
+            <FooterComp></FooterComp>
         </div>
-        
-        
-        
-        
-        
-        
-        
-        <FooterComp></FooterComp>
-      </div>
     );
-  }
+}
